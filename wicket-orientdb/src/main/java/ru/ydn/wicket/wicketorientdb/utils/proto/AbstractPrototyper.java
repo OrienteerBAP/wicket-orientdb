d4c21b14acd85fcb8a70638eef3c92f2e3f9ab88
Ilia Naryzhny <phantom@ydn.ru>
2014-07-09 02:30:21 +0400

    Fix OClassPrototyper to reflect reality with creating of OClass

--- wicket-orientdb/src/main/java/ru/ydn/wicket/wicketorientdb/utils/proto/AbstractPrototyper.java
+++ wicket-orientdb/src/main/java/ru/ydn/wicket/wicketorientdb/utils/proto/AbstractPrototyper.java
@@ -1,18 +1,23 @@
 package ru.ydn.wicket.wicketorientdb.utils.proto;
 
+import java.io.Serializable;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.wicket.Application;
 import org.apache.wicket.Session;
+import org.apache.wicket.WicketRuntimeException;
 import org.apache.wicket.core.util.lang.PropertyResolver;
 import org.apache.wicket.core.util.lang.PropertyResolverConverter;
 
+import com.google.common.primitives.Primitives;
 import com.google.common.reflect.AbstractInvocationHandler;
 
-public abstract class AbstractPrototyper<T> extends AbstractInvocationHandler {
+public abstract class AbstractPrototyper<T> extends AbstractInvocationHandler implements Serializable {
 	private static final String GET = "get";
 	private static final String IS = "is";
 	private static final String SET = "set";
@@ -43,12 +48,12 @@ public abstract class AbstractPrototyper<T> extends AbstractInvocationHandler {
 		if(methodName.startsWith(GET) && args.length==0)
 		{
 			String propName = methodName.substring(3, 4).toLowerCase()+methodName.substring(4);
-			return handleGet(propName);
+			return handleGet(propName, method.getReturnType());
 		}
 		else if(methodName.startsWith(IS) && args.length==0)
 		{
 			String propName = methodName.substring(2, 3).toLowerCase()+methodName.substring(3);
-			return handleGet(propName);
+			return handleGet(propName, method.getReturnType());
 		}
 		else if(methodName.startsWith(SET) &&  args.length==1)
 		{
@@ -65,10 +70,14 @@ public abstract class AbstractPrototyper<T> extends AbstractInvocationHandler {
 	protected T handleRealize()
 	{
 		T ret = createInstance();
-		PropertyResolverConverter prc = new PropertyResolverConverter(Application.get().getConverterLocator(),
-			Session.get().getLocale());
 		for (Map.Entry<String, Object> entry: values.entrySet()) {
-			PropertyResolver.setValue(entry.getKey(), ret, entry.getValue(), prc);
+			try
+			{
+				PropertyResolver.setValue(entry.getKey(), ret, entry.getValue(), null);
+			} catch (WicketRuntimeException e)
+			{
+				// NOP
+			}
 		}
 		realized = ret;
 		return ret;
@@ -78,9 +87,48 @@ public abstract class AbstractPrototyper<T> extends AbstractInvocationHandler {
 	
 	protected abstract Class<T> getMainInterface();
 	
-	protected Object handleGet(String propName)
+	protected Class<?>[] getAdditionalInterfaces()
 	{
-		return values.get(propName);
+		return null;
+	}
+	
+	protected Object handleGet(String propName, Class<?> returnType)
+	{
+		Object ret = values.get(propName);
+		if(ret==null)
+		{
+			ret = getDefaultValue(propName, returnType);
+			if(ret!=null) values.put(propName, ret);
+		}
+		return ret;
+	}
+	
+	protected Object getDefaultValue(String propName, Class<?> returnType)
+	{
+		Object ret = null;
+		if(returnType.isPrimitive())
+		{
+			if(returnType.equals(boolean.class))
+			{
+				return false;
+			}
+			else if(returnType.equals(char.class))
+			{
+				return '\0';
+			}
+			else
+			{
+				try
+				{
+					Class<?> wrapperClass = Primitives.wrap(returnType);
+					return wrapperClass.getMethod("valueOf", String.class).invoke(null, "0");
+				} catch (Throwable e)
+				{
+					throw new WicketRuntimeException("Can't create default value for '"+propName+"' which should have type '"+returnType.getName()+"'");
+				} 
+			}
+		}
+		return ret;
 	}
 	
 	protected void handleSet(String propName, Object value)
@@ -90,15 +138,29 @@ public abstract class AbstractPrototyper<T> extends AbstractInvocationHandler {
 	
 	protected Object handleCustom(Object proxy, Method method, Object[] args)
 	{
-		throw new UnsupportedOperationException();
+		throw new UnsupportedOperationException("Method '"+method.getName()+"' is not supported by proxy: "+this.getClass().getSimpleName());
 	}
 	
 	static <T> T newPrototype(AbstractPrototyper<T> prototype)
 	{
 		ClassLoader loader = prototype.getClass().getClassLoader();
 		Class<T> mainInterface = prototype.getMainInterface();
-		Object ret = Proxy.newProxyInstance(loader, new Class<?>[]{mainInterface, IPrototype.class}, prototype);
+		Class<?>[] interfaces = new Class<?>[]{mainInterface, IPrototype.class};
+		Class<?>[] addon = prototype.getAdditionalInterfaces();
+		if(addon!=null && addon.length>0)
+		{
+			Class<?>[] newInterfaces = new Class<?>[interfaces.length+addon.length];
+			System.arraycopy(interfaces, 0, newInterfaces, 0, interfaces.length);
+			System.arraycopy(addon, 0, newInterfaces, interfaces.length, addon.length);
+			interfaces = newInterfaces;
+		}
+		Object ret = Proxy.newProxyInstance(loader, interfaces, prototype);
 		return mainInterface.cast(ret);
+	}
+	
+	@Override
+	public String toString() {
+		return "Prototype for '"+getMainInterface().getName()+"'";
 	}
 
 }
--- wicket-orientdb/src/main/java/ru/ydn/wicket/wicketorientdb/utils/proto/OClassPrototyper.java
+++ wicket-orientdb/src/main/java/ru/ydn/wicket/wicketorientdb/utils/proto/OClassPrototyper.java
@@ -1,16 +1,33 @@
 package ru.ydn.wicket.wicketorientdb.utils.proto;
 
+import java.lang.reflect.Method;
+import java.util.Collections;
+
 import ru.ydn.wicket.wicketorientdb.OrientDbWebSession;
 
 import com.orientechnologies.orient.core.metadata.schema.OClass;
 import com.orientechnologies.orient.core.metadata.schema.OSchema;
+import com.orientechnologies.orient.core.metadata.schema.OSchemaShared;
+import com.orientechnologies.orient.core.metadata.schema.clusterselection.OClusterSelectionStrategy;
 
 public class OClassPrototyper extends AbstractPrototyper<OClass> {
+	
+	public static interface OClassSetNameFix
+	{
+		public OClass setName(String iName);
+	}
+	
+	private static final Class<?>[] FIX_INTERFACES = new Class<?>[]{OClassSetNameFix.class}; 
+	
+
+	public OClassPrototyper()
+	{
+		values.put("overSize", (float)0);
+	}
 
 	@Override
 	protected OClass createInstance() {
 		OSchema schema = OrientDbWebSession.get().getDatabase().getMetadata().getSchema();
-		schema.createClass(values.get("name").toString());
 		return schema.createClass(values.get("name").toString());
 	}
 
@@ -23,5 +40,61 @@ public class OClassPrototyper extends AbstractPrototyper<OClass> {
 	{
 		return newPrototype(new OClassPrototyper());
 	}
+	
+	
+	
+
+	@Override
+	protected Class<?>[] getAdditionalInterfaces() {
+		return FIX_INTERFACES;
+	}
+	
+	
+
+	@Override
+	protected void handleSet(String propName, Object value) {
+		if("clusterSelection".equals(propName))
+		{
+			if(value instanceof OClusterSelectionStrategy)
+			{
+				super.handleSet(propName, value);
+			}
+			else if(value instanceof CharSequence)
+			{
+				OSchema schema = OrientDbWebSession.get().getDatabase().getMetadata().getSchema();
+				
+				if(schema instanceof OSchemaShared)
+				{
+					super.handleSet(propName, ((OSchemaShared)schema).getClusterSelectionFactory().newInstance(value.toString()));
+				}
+			}
+		}
+		else
+		{
+			super.handleSet(propName, value);
+		}
+	}
+
+	@Override
+	protected Object handleCustom(Object proxy, Method method, Object[] args) {
+		String methodName = method.getName();
+		if("properties".equals(methodName) || "declaredProperties".equals(methodName))
+		{
+			return Collections.EMPTY_SET;
+		}
+		else
+		{
+			return super.handleCustom(proxy, method, args);
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "Prototype for '"+getMainInterface().getName()+"'";
+	}
+	
+	
+	
+	
 
 }
