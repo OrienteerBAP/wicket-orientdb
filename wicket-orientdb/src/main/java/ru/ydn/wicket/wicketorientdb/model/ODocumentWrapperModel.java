3bdce12e2d0095d8bff6e54ff5da2d8ded989263
Ilia Naryzhny <phantom@ydn.ru>
2014-11-18 14:40:36 -0800

    Special model for ODocumentWrappers

---
+++ wicket-orientdb/src/main/java/ru/ydn/wicket/wicketorientdb/model/ODocumentWrapperModel.java
@@ -0,0 +1,49 @@
+package ru.ydn.wicket.wicketorientdb.model;
+
+import org.apache.wicket.model.Model;
+
+import com.orientechnologies.orient.core.type.ODocumentWrapper;
+
+public class ODocumentWrapperModel<T extends ODocumentWrapper> extends Model<T>
+{
+	private static final long serialVersionUID = 1L;
+	
+	private boolean needToReload=false;
+	public ODocumentWrapperModel()
+	{
+		super();
+	}
+
+	public ODocumentWrapperModel(T object)
+	{
+		super(object);
+		needToReload=false;
+	}
+	
+
+	@Override
+	public T getObject() {
+		T ret=  super.getObject();
+		if(ret!=null && needToReload)
+		{
+			ret.reload();
+			needToReload = false;
+		}
+		return ret;
+	}
+
+	@Override
+	public void setObject(T object) {
+		super.setObject(object);
+		needToReload = false;
+	}
+
+	@Override
+	public void detach() {
+		needToReload = true;
+		super.detach();
+	}
+	
+	
+	
+}
--- wicket-orientdb/src/main/java/ru/ydn/wicket/wicketorientdb/model/OQueryDataProvider.java
+++ wicket-orientdb/src/main/java/ru/ydn/wicket/wicketorientdb/model/OQueryDataProvider.java
@@ -11,6 +11,7 @@ import org.apache.wicket.model.Model;
 
 import com.google.common.base.Function;
 import com.orientechnologies.orient.core.record.impl.ODocument;
+import com.orientechnologies.orient.core.type.ODocumentWrapper;
 
 /**
  * Provider of data by quering of OrientDB
@@ -73,6 +74,7 @@ public class OQueryDataProvider <K> extends SortableDataProvider<K, String>
 	public IModel<K> model(K o)
     {
     	if(o instanceof ODocument) return (IModel<K>)new ODocumentModel((ODocument)o);
+    	else if(o instanceof ODocumentWrapper) return (IModel<K>)new ODocumentWrapperModel<ODocumentWrapper>((ODocumentWrapper)o);
     	else if(o instanceof Serializable) return (IModel<K>)Model.of((Serializable)o);
     	else throw new WicketRuntimeException(OQueryDataProvider.class.getSimpleName()+" can't work with non serializable objects: "+o);
     }
