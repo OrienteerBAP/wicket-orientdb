a12db021a3ed9416e84ca40ccb4c3e9d547f3700
Ilia Naryzhny <phantom@ydn.ru>
2015-01-12 18:29:52 -0800

    Add failed cases in orientdb

--- wicket-orientdb/src/test/java/ru/ydn/wicket/wicketorientdb/orientdb/TestInAppOrientDBCompatibility.java
+++ wicket-orientdb/src/test/java/ru/ydn/wicket/wicketorientdb/orientdb/TestInAppOrientDBCompatibility.java
@@ -7,9 +7,13 @@ import org.junit.Test;
 import ru.ydn.wicket.wicketorientdb.junit.WicketOrientDbTesterScope;
 import static org.junit.Assert.*;
 
+import com.orientechnologies.orient.core.db.ODatabase;
+import com.orientechnologies.orient.core.db.document.ODatabaseDocument;
 import com.orientechnologies.orient.core.metadata.schema.OClass;
+import com.orientechnologies.orient.core.metadata.schema.OProperty;
 import com.orientechnologies.orient.core.metadata.schema.OSchema;
 import com.orientechnologies.orient.core.metadata.schema.OType;
+import com.orientechnologies.orient.core.record.impl.ODocument;
 
 public class TestInAppOrientDBCompatibility
 {
@@ -27,5 +31,50 @@ public class TestInAppOrientDBCompatibility
 		OClass classB = schema.createClass("TestExistsB", classA);
 		assertNotNull(classB.getProperty("property"));
 		assertTrue(classB.existsProperty("property"));
+	}
+	
+	@Test
+	@Ignore
+	public void testRemovingReadonlyField()
+	{
+		ODatabaseDocument db = wicket.getTester().getDatabase();
+		OSchema schema = db.getMetadata().getSchema();
+		OClass classA = schema.createClass("TestRemovingField");
+		classA.createProperty("name", OType.STRING);
+		OProperty property = classA.createProperty("property", OType.STRING);
+		property.setReadonly(true);
+		
+		ODocument doc = new ODocument(classA);
+		doc.field("name", "My Name");
+		doc.field("property", "value1");
+		doc.save();
+		
+		doc.field("name", "My Name 2");
+		doc.field("property", "value2");
+		doc.removeField("property");
+		doc.save();
+	}
+	
+	@Test
+	@Ignore
+	public void testRemovingReadonlyField2()
+	{
+		ODatabaseDocument db = wicket.getTester().getDatabase();
+		OSchema schema = db.getMetadata().getSchema();
+		OClass classA = schema.createClass("TestRemovingField2");
+		classA.createProperty("name", OType.STRING);
+		OProperty property = classA.createProperty("property", OType.STRING);
+		property.setReadonly(true);
+		
+		ODocument doc = new ODocument(classA);
+		doc.field("name", "My Name");
+		doc.field("property", "value1");
+		doc.save();
+		
+		doc.field("name", "My Name 2");
+		doc.field("property", "value2");
+		doc.undo();
+		doc.field("name", "My Name 3");
+		doc.save();
 	}
 }
