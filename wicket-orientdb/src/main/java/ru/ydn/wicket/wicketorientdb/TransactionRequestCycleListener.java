a2839ec52032895da46511ce0bde98b4c877f4b7
Ilia Naryzhny <phantom@ydn.ru>
2015-07-17 10:59:45 -0700

    Corrections for db.getUser() - additional checks for null

--- wicket-orientdb/src/main/java/ru/ydn/wicket/wicketorientdb/TransactionRequestCycleListener.java
+++ wicket-orientdb/src/main/java/ru/ydn/wicket/wicketorientdb/TransactionRequestCycleListener.java
@@ -6,6 +6,7 @@ import org.apache.wicket.request.cycle.RequestCycle;
 import org.apache.wicket.request.handler.resource.ResourceReferenceRequestHandler;
 import org.apache.wicket.request.resource.PackageResourceReference;
 import org.apache.wicket.request.resource.ResourceReference;
+import org.apache.wicket.util.lang.Objects;
 
 import com.orientechnologies.orient.core.db.ODatabaseRecordThreadLocal;
 import com.orientechnologies.orient.core.db.document.ODatabaseDocument;
@@ -22,8 +23,11 @@ public class TransactionRequestCycleListener extends
 		OrientDbWebSession session = OrientDbWebSession.get();
 		ODatabaseDocument db = session.getDatabase();
 		//It's required to have ability to check security rights locally
-		OSecurityUser oUser = db.getUser();
-		if(oUser.getDocument()!=null && oUser.getDocument().getIdentity()!=null && !oUser.getDocument().getIdentity().isValid())
+		OSecurityUser oUser = session.getUser();
+		OSecurityUser dbUser = db.getUser();
+		if(oUser!=null && oUser.getDocument()!=null 
+				&& oUser.getDocument().getIdentity()!=null 
+				&& (!oUser.getDocument().getIdentity().isValid() || dbUser==null || !Objects.equal(dbUser.getName(), oUser.getName())))
 		{
 			db.setUser(db.getMetadata().getSecurity().getUser(oUser.getName()));
 		}
