daa41590b2e4568c4857937806422e4046c15282
Ilia Naryzhny <phantom@ydn.ru>
2015-08-29 00:32:31 -0700

    DisableIfDocumentNotSavedBehavior should check transaction

--- wicket-orientdb/src/main/java/ru/ydn/wicket/wicketorientdb/behavior/DisableIfDocumentNotSavedBehavior.java
+++ wicket-orientdb/src/main/java/ru/ydn/wicket/wicketorientdb/behavior/DisableIfDocumentNotSavedBehavior.java
@@ -3,7 +3,12 @@ package ru.ydn.wicket.wicketorientdb.behavior;
 import org.apache.wicket.Component;
 import org.apache.wicket.behavior.Behavior;
 
+import ru.ydn.wicket.wicketorientdb.OrientDbWebSession;
+
 import com.orientechnologies.orient.core.db.record.OIdentifiable;
+import com.orientechnologies.orient.core.db.record.ORecordOperation;
+import com.orientechnologies.orient.core.id.ORID;
+import com.orientechnologies.orient.core.tx.OTransaction;
 
 /**
  * {@link Behavior} to disable component if {@link OIdentifiable} was not saved in DB
@@ -17,7 +22,15 @@ public class DisableIfDocumentNotSavedBehavior extends Behavior {
 		super.onConfigure(component);
 		Object object = component.getDefaultModelObject();
 		if(object!=null && object instanceof OIdentifiable) {
-			component.setEnabled(((OIdentifiable)object).getIdentity().isPersistent());
+			ORID rid = ((OIdentifiable)object).getIdentity();
+			if(rid.isPersistent()) {
+				component.setEnabled(true);
+			} else {
+				// Is record scheduled for creation?
+				OTransaction transaction = OrientDbWebSession.get().getDatabase().getTransaction();
+				ORecordOperation operation = transaction.getRecordEntry(rid);
+				component.setEnabled(operation!=null && operation.type==ORecordOperation.CREATED);
+			}
 		}
 	}
 }
