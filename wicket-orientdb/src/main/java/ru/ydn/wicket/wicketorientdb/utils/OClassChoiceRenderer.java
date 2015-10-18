2efca5277b73371485af724b9fcc0d8b6b45fc10
Ilia Naryzhny <phantom@ydn.ru>
2015-10-17 22:55:26 -0700

    Move to Wicket 7: fix #34

---
+++ wicket-orientdb/src/main/java/ru/ydn/wicket/wicketorientdb/utils/OClassChoiceRenderer.java
@@ -0,0 +1,39 @@
+package ru.ydn.wicket.wicketorientdb.utils;
+
+import java.util.List;
+
+import org.apache.wicket.markup.html.form.IChoiceRenderer;
+import org.apache.wicket.model.IModel;
+
+import ru.ydn.wicket.wicketorientdb.OrientDbWebSession;
+import ru.ydn.wicket.wicketorientdb.model.OClassNamingModel;
+
+import com.orientechnologies.orient.core.metadata.schema.OClass;
+
+/**
+ * {@link IChoiceRenderer} for {@link OClass}es
+ */
+public class OClassChoiceRenderer implements IChoiceRenderer<OClass> {
+	
+	public static final OClassChoiceRenderer INSTANCE = new OClassChoiceRenderer();
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public Object getDisplayValue(OClass object) {
+		return new OClassNamingModel(object).getObject();
+	}
+
+	@Override
+	public String getIdValue(OClass object, int index) {
+		return object.getName();
+	}
+
+	@Override
+	public OClass getObject(String id,
+			IModel<? extends List<? extends OClass>> choicesModel) {
+		OClass ret = OrientDbWebSession.get().getDatabase().getMetadata().getSchema().getClass(id);
+		List<? extends OClass> choices = choicesModel.getObject();
+		return choices!=null && choices.contains(ret) ? ret : null;
+	}
+}
