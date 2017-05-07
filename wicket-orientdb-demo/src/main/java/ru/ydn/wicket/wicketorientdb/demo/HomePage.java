590d41c41c7f0570070995dab1448f9697d29397
Ilia Naryzhny <phantom@ydn.ru>
2017-05-07 14:58:55 -0700

    Add sample of table into demo

--- wicket-orientdb-demo/src/main/java/ru/ydn/wicket/wicketorientdb/demo/HomePage.java
+++ wicket-orientdb-demo/src/main/java/ru/ydn/wicket/wicketorientdb/demo/HomePage.java
@@ -1,11 +1,22 @@
 package ru.ydn.wicket.wicketorientdb.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.wicket.authroles.authentication.panel.SignInPanel;
 import org.apache.wicket.devutils.debugbar.DebugBar;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.DefaultDataTable;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+import com.orientechnologies.orient.core.record.impl.ODocument;
+
+import ru.ydn.wicket.wicketorientdb.components.table.DocumentPropertyColumn;
+import ru.ydn.wicket.wicketorientdb.model.OQueryDataProvider;
 
 /**
  * Demo home page
@@ -24,5 +35,12 @@ public class HomePage extends WebPage {
 		add(new Label("signedInUser", new PropertyModel<String>(this, "session.user.name")));
 //		((OrientDbWebSession)getSession()).getDatabase().getUser().
 		add(new SignInPanel("signInPanel"));
+		OQueryDataProvider<ODocument> provider = new OQueryDataProvider<>("select from "+WicketApplication.CLASS_NAME);
+		List<IColumn<ODocument, String>> columns = new ArrayList<>();
+		columns.add(new DocumentPropertyColumn(Model.of(WicketApplication.PROP_NAME), WicketApplication.PROP_NAME, WicketApplication.PROP_NAME));
+		columns.add(new DocumentPropertyColumn(Model.of(WicketApplication.PROP_INT), WicketApplication.PROP_INT, WicketApplication.PROP_INT));
+		columns.add(new DocumentPropertyColumn(Model.of(WicketApplication.PROP_DATE), WicketApplication.PROP_DATE, WicketApplication.PROP_DATE));
+		DefaultDataTable<ODocument, String> table = new DefaultDataTable<>("table", columns, provider, 15);
+		add(table);
     }
 }
--- wicket-orientdb-demo/src/main/java/ru/ydn/wicket/wicketorientdb/demo/WicketApplication.java
+++ wicket-orientdb-demo/src/main/java/ru/ydn/wicket/wicketorientdb/demo/WicketApplication.java
@@ -1,14 +1,24 @@
 package ru.ydn.wicket.wicketorientdb.demo;
 
+import java.sql.Date;
+import java.util.Random;
+
 import org.apache.wicket.authroles.authentication.pages.SignInPage;
 import org.apache.wicket.markup.html.WebPage;
 
+import ru.ydn.wicket.wicketorientdb.AbstractDataInstallator;
 import ru.ydn.wicket.wicketorientdb.EmbeddOrientDbApplicationListener;
 import ru.ydn.wicket.wicketorientdb.IOrientDbSettings;
 import ru.ydn.wicket.wicketorientdb.OrientDbWebApplication;
+import ru.ydn.wicket.wicketorientdb.utils.OSchemaHelper;
 
 import com.orientechnologies.orient.client.remote.OServerAdmin;
+import com.orientechnologies.orient.core.db.document.ODatabaseDocument;
 import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
+import com.orientechnologies.orient.core.metadata.schema.OClass;
+import com.orientechnologies.orient.core.metadata.schema.OSchema;
+import com.orientechnologies.orient.core.metadata.schema.OType;
+import com.orientechnologies.orient.core.record.impl.ODocument;
 
 /**
  * Application object for your web application.
@@ -16,6 +26,11 @@ import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
 public class WicketApplication extends OrientDbWebApplication
 {
 	public static final String DB_NAME = "WicketOrientDb";
+	public static final String CLASS_NAME = "TestData";
+	public static final String PROP_NAME = "name";
+	public static final String PROP_INT = "number";
+	public static final String PROP_DATE = "date";
+	
 	/**
 	 * @see org.apache.wicket.Application#getHomePage()
 	 */
@@ -50,6 +65,31 @@ public class WicketApplication extends OrientDbWebApplication
 		getOrientDbSettings().setDBUrl("memory:"+DB_NAME);
 		getOrientDbSettings().setGuestUserName("admin");
 		getOrientDbSettings().setGuestPassword("admin");
+		getApplicationListeners().add(new AbstractDataInstallator() {
+			
+			
+			@Override
+			protected void installData(OrientDbWebApplication app, ODatabaseDocument db) {
+				OSchemaHelper helper = OSchemaHelper.bind(db);
+				helper.oClass(CLASS_NAME)
+						.oProperty(PROP_NAME, OType.STRING)
+						.oProperty(PROP_INT, OType.INTEGER)
+						.oProperty(PROP_DATE, OType.DATE);
+				if(helper.getOClass().count()==0) {
+					Random random = new Random();
+					Date today = new Date(System.currentTimeMillis());
+					int delta = 365*24*60*60;
+					for (int i=0; i<50; i++) {
+						ODocument doc = new ODocument(helper.getOClass());
+						doc.field(PROP_NAME, "Name for #"+i);
+						doc.field(PROP_INT, i);
+						doc.field(PROP_DATE, new Date(today.getTime()+(random.nextInt(2*delta)-delta)*1000));
+						doc.save();
+					}
+				}
+				
+			}
+		});
 	}
 
 	@Override
