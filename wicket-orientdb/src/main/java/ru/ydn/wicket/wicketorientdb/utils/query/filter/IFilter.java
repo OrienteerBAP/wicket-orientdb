964e8409f2a052b8c16bfb53b130a62aecb88a78
Vitaliy Gonchar <weaxme@gmail.com>
2017-07-03 13:17:20 +0300

    Fix serialize exception

--- wicket-orientdb/src/main/java/ru/ydn/wicket/wicketorientdb/utils/query/filter/IFilter.java
+++ wicket-orientdb/src/main/java/ru/ydn/wicket/wicketorientdb/utils/query/filter/IFilter.java
@@ -1,8 +1,10 @@
 package ru.ydn.wicket.wicketorientdb.utils.query.filter;
 
+import org.apache.wicket.util.io.IClusterable;
+
 /**
  * Interface for generating SQL for number fields
  */
-public interface IFilter {
+public interface IFilter extends IClusterable {
     public String apply();
 }
--- wicket-orientdb/src/main/java/ru/ydn/wicket/wicketorientdb/utils/query/filter/IFilterCriteria.java
+++ wicket-orientdb/src/main/java/ru/ydn/wicket/wicketorientdb/utils/query/filter/IFilterCriteria.java
@@ -1,9 +1,11 @@
 package ru.ydn.wicket.wicketorientdb.utils.query.filter;
 
+import org.apache.wicket.util.io.IClusterable;
+
 /**
  * Interface for save filter criteria and generate SQL depending on filter criteria
  */
-public interface IFilterCriteria {
+public interface IFilterCriteria extends IClusterable {
     public String apply(String sql);
     public IFilterCriteria setFilter(IFilter filter);
 }
