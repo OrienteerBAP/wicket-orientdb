22a0866e16728820b50e49a5a368476595cef4e1
Ilia Naryzhny <phantom@ydn.ru>
2018-06-06 22:19:03 -0700

    Make RequiredOrientResource annotation repeatable

--- wicket-orientdb/src/main/java/ru/ydn/wicket/wicketorientdb/security/RequiredOrientResource.java
+++ wicket-orientdb/src/main/java/ru/ydn/wicket/wicketorientdb/security/RequiredOrientResource.java
@@ -1,5 +1,6 @@
 package ru.ydn.wicket.wicketorientdb.security;
 
+
 import org.apache.wicket.authorization.Action;
 /**
  * Annotation for specification of required OrientDB resources
@@ -7,6 +8,7 @@ import org.apache.wicket.authorization.Action;
 @java.lang.annotation.Target(value = {java.lang.annotation.ElementType.TYPE})
 @java.lang.annotation.Retention(value = java.lang.annotation.RetentionPolicy.RUNTIME)
 @java.lang.annotation.Documented
+@java.lang.annotation.Repeatable(RequiredOrientResources.class)
 public @interface RequiredOrientResource {
 	String value();
 	String specific() default "";
