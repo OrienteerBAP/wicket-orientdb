11a73e24b3d6a94039860e9c1e3a9c932fbae8f9
Vitalii Honchar <weaxme@gmail.com>
2019-11-15 15:28:47 +0200

    Fixed test OrientDB lifecycle

--- wicket-orientdb/src/test/java/ru/ydn/wicket/wicketorientdb/orientdb/TestStandaloneOrientDBCompatibility.java
+++ wicket-orientdb/src/test/java/ru/ydn/wicket/wicketorientdb/orientdb/TestStandaloneOrientDBCompatibility.java
@@ -2,6 +2,8 @@ package ru.ydn.wicket.wicketorientdb.orientdb;
 
 import static org.junit.Assert.assertNotNull;
 
+import com.orientechnologies.orient.core.db.ODatabaseType;
+import com.orientechnologies.orient.core.db.OrientDB;
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -16,90 +18,71 @@ import com.orientechnologies.orient.core.exception.OStorageException;
 import com.orientechnologies.orient.server.OServer;
 import com.orientechnologies.orient.server.OServerMain;
 
-public class TestStandaloneOrientDBCompatibility
-{
-	
-	private OServer startServer() throws Exception
-	{
-		OServer server = OServerMain.create();
-//		server.startup();
-		server.startup(OrientDbTestWebApplication.class.getResource("db.config.xml").openStream());
-		server.activate();
-		return server;
-	}
-	
-	final String PLOCAL_DB_NAME = "plocal:testDBLifeCycle";
-	final String MEMORY_DB_NAME = "memory:testDBLifeCycle";
+public class TestStandaloneOrientDBCompatibility {
+
+	private static final String PLOCAL_DB_NAME = "testDBLifeCycleLocal";
+	private static final String MEMORY_DB_NAME = "testDBLifeCycleMemory";
 	
 	@Test
-	public void testPLocalDbLifeCycle() throws Exception
-	{
-		testOrientDbLifeCycle(PLOCAL_DB_NAME, true, false);
-		testOrientDbLifeCycle(PLOCAL_DB_NAME, false, false);
-		testOrientDbLifeCycle(PLOCAL_DB_NAME, false, true);
-		testOrientDbLifeCycle(PLOCAL_DB_NAME, true, false);
-		testOrientDbLifeCycle(PLOCAL_DB_NAME, false, true);
+	public void testPLocalDbLifeCycle() throws Exception {
+		testOrientDbLifeCycle(PLOCAL_DB_NAME, ODatabaseType.PLOCAL, true, false);
+		testOrientDbLifeCycle(PLOCAL_DB_NAME, ODatabaseType.PLOCAL, false, false);
+		testOrientDbLifeCycle(PLOCAL_DB_NAME, ODatabaseType.PLOCAL, false, true);
+		testOrientDbLifeCycle(PLOCAL_DB_NAME, ODatabaseType.PLOCAL, true, false);
+		testOrientDbLifeCycle(PLOCAL_DB_NAME, ODatabaseType.PLOCAL, false, true);
 	}
 	
 	@Test
 	@Ignore
-	public void testPLocalDbLifeCycleSVT() throws Exception
-	{
-		for(int i=0;i<5;i++)
-			testOrientDbLifeCycle(PLOCAL_DB_NAME, true, true);
+	public void testPLocalDbLifeCycleSVT() throws Exception {
+		for (int i = 0; i < 5; i++) {
+			testOrientDbLifeCycle(PLOCAL_DB_NAME, ODatabaseType.PLOCAL, true, true);
+		}
 	}
 	
 	@Test
 	@Ignore
-	public void testMemoryDbLifeCycle() throws Exception
-	{
-		for(int i=0;i<5;i++)
-			testOrientDbLifeCycle(MEMORY_DB_NAME, true, true);
+	public void testMemoryDbLifeCycle() throws Exception {
+		for (int i = 0; i < 5; i++) {
+			testOrientDbLifeCycle(MEMORY_DB_NAME, ODatabaseType.MEMORY, true, true);
+		}
 	}
 	
 	@Test(expected=OStorageException.class)
 	@Ignore
-	public void testMemoryDBShouldDisapear() throws Exception
-	{
-		try
-		{
-			testOrientDbLifeCycle(MEMORY_DB_NAME, true, false);
-			testOrientDbLifeCycle(MEMORY_DB_NAME, false, true);
-		} finally
-		{
+	public void testMemoryDBShouldDisapear() throws Exception {
+		try {
+			testOrientDbLifeCycle(MEMORY_DB_NAME, ODatabaseType.MEMORY, true, false);
+			testOrientDbLifeCycle(MEMORY_DB_NAME, ODatabaseType.MEMORY, false, true);
+		} finally {
 			OServer server = OServerMain.server();
 			if(server!=null) server.shutdown();
 			Orient.instance().shutdown();
 		}
 	}
 	
-	public void testOrientDbLifeCycle(String dbURL, boolean createDb, boolean dropDb) throws Exception
-	{
+	public void testOrientDbLifeCycle(String dbName, ODatabaseType type, boolean createDb, boolean dropDb) throws Exception {
 		Orient.instance().startup();
 		assertNotNull(ODatabaseRecordThreadLocal.instance());
 		Orient.instance().removeShutdownHook();
 		OServer server = OServerMain.create();
 		server.startup(OrientDbTestWebApplication.class.getResource("db.config.xml").openStream());
 		server.activate();
-		if(createDb)
-		{
-			ODatabaseDocument dbToCreate = new ODatabaseDocumentTx(dbURL);
-			if(!dbToCreate.exists()) dbToCreate.create();
-			dbToCreate.close();
+
+		OrientDB orientDB = server.getContext();
+
+		if (createDb && !orientDB.exists(dbName)) {
+			orientDB.create(dbName, type);
 		}
 		assertNotNull(ODatabaseRecordThreadLocal.instance());
-		ODatabaseDocument db = new OPartitionedDatabasePoolFactory().get(dbURL, "admin", "admin").acquire();
+		ODatabaseDocument db = orientDB.cachedPool(dbName, "admin", "admin").acquire();
 		db.close();
 		assertNotNull(ODatabaseRecordThreadLocal.instance());
-		if(dropDb)
-		{
-			ODatabaseDocument dbToDrop = new ODatabaseDocumentTx(dbURL);
-			dbToDrop.open("admin", "admin");
-			dbToDrop.drop();
+		if (dropDb) {
+			orientDB.drop(dbName);
 		}
 		server.shutdown();
 		Orient.instance().shutdown();
-//		Thread.sleep(50);
 	}
-	
+
 }
