c47aeef173a35366c38213fdd8ff6467e694586f
Ilia Naryzhny <phantom@ydn.ru>
2020-04-12 15:16:07 -0700

    Exclude standalone tests because they make test very slow

--- wicket-orientdb/src/test/java/ru/ydn/wicket/wicketorientdb/orientdb/TestStandaloneOrientDBCompatibility.java
+++ wicket-orientdb/src/test/java/ru/ydn/wicket/wicketorientdb/orientdb/TestStandaloneOrientDBCompatibility.java
@@ -14,6 +14,7 @@ import ru.ydn.wicket.wicketorientdb.OrientDbTestWebApplication;
 
 import static org.junit.Assert.assertNotNull;
 
+@Ignore
 public class TestStandaloneOrientDBCompatibility {
 
 	private static final String PLOCAL_DB_NAME = "testDBLifeCycleLocal";
